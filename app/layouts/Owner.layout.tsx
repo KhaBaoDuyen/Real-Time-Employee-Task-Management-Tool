338322690a195dfd6f63251f4f0594a21d69d0cc
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2026-02-05 15:18:58 +0700

    feat(layout): add owner layout structure

---
+++ app/layouts/Owner.layout.tsx
@@ -0,0 +1,21 @@
+import { Outlet, useLocation } from "react-router";
+import { Sidebar } from "~/components/layout/Sidebar/Sidebar";
+import { Topbar } from "~/components/layout/Topbar/Topbar";
+
+export default function AdminLayout() {
+    const location = useLocation();
+    return (
+        <div className="flex min-h-screen bg-base">
+            <Sidebar />
+
+            <div className="flex flex-1 pl-0 pt-0  flex-col">
+                <div className="w-11/12 mx-auto">
+                    <Topbar />
+                </div>
+                <main className="flex-1 w-11/12 mx-auto rounded-2xl">
+                    <Outlet key={location.pathname} />
+                </main>
+            </div>
+        </div>
+    );
+}
