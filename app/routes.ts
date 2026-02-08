9e98933ee22c19329f4da9bc5a1c60b0e06267a7
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2026-02-08 19:59:19 +0700

    chore: update routes and project config

--- app/routes.ts
+++ app/routes.ts
@@ -6,9 +6,11 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  index("routes/auth/login/login.staff.tsx"),
-  //    route("login", "routes/auth/login.tsx"),
-  //   route("verify", "routes/auth/verify.tsx"),
+  layout("layouts/auth.layout.tsx", [
+    index("routes/auth/login.staff.tsx"),
+    route("/owner", "routes/auth/login.owner.tsx"),
+    route("/verify-otp", "routes/auth/verify.otp.tsx"),
+  ]),
 
   layout("layouts/owner.layout.tsx", [
     route("owner/staff/list", "routes/owner/staff/staff.list.tsx"),
